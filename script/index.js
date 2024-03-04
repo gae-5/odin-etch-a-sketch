eefce01c2ea5b99fdf1b233132b4f1d0d80dc6af
Osur <43894197+gae-5@users.noreply.github.com>
2024-03-04 13:31:28 +0300

    Add grid size update button
    The button updates the size of the grid and also ensures that the
    size specified by the user does not exceed 100. This is to ensure that
    the computer resources used to generated the grid are not exhausted, leading to
    problems like the computer hanging, freezing or crashing.

--- script/index.js
+++ script/index.js
@@ -2,14 +2,17 @@ const mainWrapper = document.querySelector("div.main-container");
 const grid = document.createElement("div");
 grid.className ='grid';
 
-// create 16 grid items.
-let gridItem;
-for(let i=0; i<256; i++){
-    gridItem = document.createElement("div");
-    gridItem.className="grid-item";
-    grid.appendChild(gridItem);
+const gridBtn = document.createElement("button");
+gridBtn.textContent = "Update grid";
+mainWrapper.appendChild(gridBtn);
+
+
+let gridSize = 16;
+let gridItemsTotal = gridSize * gridSize;
+function updateGrid(){
+    let updateGridSize = Number(prompt("What is the number of squares per side for the new grid ?"));
+    return updateGridSize;
 }
-mainWrapper.appendChild(grid);
 
 function random(number){
     return Math.floor(Math.random()*number);
@@ -20,11 +23,59 @@ function changeColor(){
     return randColor;
 }
 
-
-const gridItems = document.querySelectorAll("div.grid-item");
-gridItems.forEach((gridI)=>{    
-    gridI.addEventListener("mouseover",(e)=>{        
-        e.target.style.backgroundColor = changeColor();    
+function gridItemColorChange(){
+    gridItems.forEach((gridI)=>{    
+        gridI.addEventListener("mouseover",(e)=>{        
+            e.target.style.backgroundColor = changeColor();    
+        });
     });
+}
+
+// create 16 grid items.
+let gridItem;
+for(let i=0; i<gridItemsTotal; i++){
+    gridItem = document.createElement("div");
+    gridItem.className="grid-item";
+    grid.appendChild(gridItem);
+}
+mainWrapper.appendChild(grid);
+let gridItems = document.querySelectorAll("div.grid-item");
+
+
+gridBtn.addEventListener("click",()=>{
+    gridSize=updateGrid();
+
+    let inputValidity = true;
+    while(inputValidity){
+        if (gridSize > 100){
+            gridSize=updateGrid();
+        } else if (gridSize === 0){
+            gridSize = 20;
+        } 
+        else{
+            inputValidity=false;
+        }
+    }
+
+    gridItemsTotal = gridSize*gridSize;
+    
+    const clearGrid = document.querySelectorAll("div.grid-item");
+    clearGrid.forEach((item) =>{
+        item.parentElement.removeChild(item);
+    });
+
+    for(let i=0; i<gridItemsTotal; i++){
+        gridItem = document.createElement("div");
+        gridItem.className="grid-item";
+        gridItem.style.height = `${100/gridSize}%`;
+        gridItem.style.width = `${100/gridSize}%`;
+        grid.appendChild(gridItem);
+    }
+    mainWrapper.appendChild(grid);
+
+    gridItems = document.querySelectorAll("div.grid-item");
+    gridItemColorChange();
+    
 });
 
+gridItemColorChange();
